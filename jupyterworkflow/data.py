eb8f3a7a0ca59c25a52e3a83e52aab4ec523abe4
dholanaresh <65899889+dholanaresh@users.noreply.github.com>
2020-06-04 19:19:23 +0200

    Move data download to jupyterworkflow package

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,13 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+
+FREMONT_URL = "https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD"
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+    if force_download or not os.path.exists(filename):
+        urlretrieve(URL,'Fremont.csv')
+    data = pd.read_csv('Fremont.csv',index_col='Date', parse_dates=True)
+    data.columns = ['Total','East','West']
+    return data
